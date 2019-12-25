ff538ac5a99eac64f8cb699ee349defca40ddd1d
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2019-12-25 23:25:55 +0530

    scrap api integrated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    url: '',
+    scrapData: null,
+    loading: false,
+    showDownloadBtn: false,
+    isError: false
+  }
+
+  handleClick = (e) => {
+    this.setState({loading:true})
+    e.preventDefault();
+    let scrapData = '';
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    let url = "https://app.zenscrape.com/api/v1/get?url=" + this.state.url + "&render=&premium=&apikey=15a8d570-2735-11ea-bad3-cd0e72860b8f";
+    axios.get(proxyurl + url)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        scrapData = response.data;
+        this.setState({ scrapData , loading: false, showDownloadBtn:true});
+        console.log('scrapData', scrapData)
+      }).catch((err)=>{
+console.log('error')
+this.setState({isError:true})
+      })
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({ [e.target.id]: e.target.value, showDownloadBtn:false, isError:false })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.loading ? 'loading': 'not loading'}
+        <form onSubmit={this.handleClick}>
+          <input type="text" onChange={this.handleChange} id="url"></input>
+          {this.state.showDownloadBtn ?  <button >download</button> :  <button >get web page</button>}
+          {this.state.isError ? 'error in url': ''}
+        </form>
+      </div>
+    )
+  }
 }
+
 
 export default App;
